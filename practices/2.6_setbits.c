5136300d661947943483ee97537b98e1c28958dc
Liu@iMac <pizza@pizzaliu.com>
2016-07-15 16:10:59 +0800

    2.6

---
+++ practices/2.6_setbits.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int setbits(int x, unsigned int p, unsigned int n, int y);
+
+int main()
+{
+    int x, y;
+    unsigned int p, n;
+
+    x = 1025;
+    p = 2;
+    n = 3;
+    y = 2;
+
+    printf("setbits(%d, %ud, %ud, %d) = %d\n", x, p, n, y, setbits(x, p, n, y));
+
+    return 0;
+}
+
+int setbits(int x, unsigned int p, unsigned int n, int y)
+{
+    int i = x & ~(~0 << n) << (p+1-n);
+    int j = (y & ~(~0 << n)) << (p+1-n);
+
+    return x - i + j;
+}
