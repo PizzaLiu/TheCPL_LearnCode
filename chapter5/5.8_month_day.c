e79ecd1770278ac29f9e8d62d5c1ee7645cdb91a
Liu@iMac <pizza@pizzaliu.com>
2016-07-25 11:30:40 +0800

    5.8

---
+++ chapter5/5.8_month_day.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+
+static char daytab[2][13] = {
+    {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31},
+    {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}
+};
+
+/* day_of_year: set day of year from month & day */
+int day_of_year(int year, int month, int day)
+{
+    int i, leap;
+
+    if (month > 12) {
+        printf("error: wrong month\n");
+        return -1;
+    }
+
+    leap = year%4 == 0 && year%100 != 0 && year%400 == 0;
+    for (i = 1; i < month; ++i)
+        day += daytab[leap][i];
+    return day;
+}
+
+/* month_day: set month, day from day of year */
+void month_day(int year, int yearday, int *pmonth, int *day)
+{
+    int i, leap;
+    leap = year%4 == 0 && year%100 != 0 && year%400 == 0;
+    if ((leap == 1 && yearday > 366) || yearday > 365) {
+        printf("error: wrong yearday\n");
+        return;
+    }
+    for (i = 1; yearday > daytab[leap][i]; ++i)
+        yearday -= daytab[leap][i];
+    *pmonth = i;
+    *day = yearday;
+}
+
+
+int main()
+{
+
+    int year = 2016;
+    int month = 9;
+    int day = 10;
+
+    int yearday = day_of_year(year, month, day);
+
+    printf("%d-%d-%d is the %d days of %d.\n", year, month, day, yearday, year);
+
+    month_day(year, ++yearday, &month, &day);
+
+    printf("the %d days of %d is %d-%d-%d\n", yearday, year, year, month, day);
+
+    return 0;
+}
